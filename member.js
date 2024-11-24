f1f8b92690d2dbb1e5cf70a852c441d9f7c291b8
Saurabh Agrawal <sagrawal@jhb.dvt.co.za>
2024-11-24 16:29:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMembers(){
+    var members = [
+        {name: "John", skills: ["JavaScript", "React", "Node"]},
+        {name: "Jane", skills: ["JavaScript", "React", "Node"]},
+        {name: "Jack", skills: ["JavaScript", "React", "Node"]}
+    ];
+    return members;
+}
